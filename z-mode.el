ef6d64e71ab60a6ea353e7a6cc5848804e4e5608
Chris Done <chrisdone@gmail.com>
2012-12-30 23:12:01 +0100

    Update emacs mode.

--- z-mode.el
+++ z-mode.el
@@ -6,11 +6,30 @@
 
 ;;; Code:
 
-(add-to-list 'auto-mode-alist '("\\.z\\'" . z-mode))
+(add-to-list 'auto-mode-alist '("\\.zz\\'" . z-mode))
 
 ;;;###autoload
 (define-derived-mode z-mode fundamental-mode "Z"
-  "Major mode for Z files.")
+  "Major mode for Z files."
+  (make-local-variable 'font-lock-defaults)
+  (setq font-lock-defaults '((z-font-lock-keywords)))
+  (set (make-local-variable 'comment-start) "-- ")
+  (set (make-local-variable 'comment-end) ""))
+
+(defconst z-keywords
+  (eval-when-compile
+    (regexp-opt
+     '("defun" "defmacro" "fn" "if" "do")))
+  "Z keywords.")
+
+(defconst z-font-lock-keywords
+  (list
+   ;; Fontify keywords
+   (cons
+    (concat "\\<\\(" z-keywords "\\)\\>")
+    '(1 font-lock-keyword-face))
+   '("^-- .*" . font-lock-comment-face))
+  "Subdued level highlighting for z mode.")
 
 (provide 'z-mode)
 ;;; z.el ends here
