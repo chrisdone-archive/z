2eb2d17504b0cef3ed532a9da3bc61067503b605
Chris Done <chrisdone@gmail.com>
2013-01-01 00:52:37 +0100

    Remove redundant stuff.

--- src/Z.hs
+++ src/Z.hs
@@ -3,29 +3,6 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 -- A little language called Z.
---
--- Examples:
---
--- fn x y
---    fn p
---       do print p
---          print if = p
---                     42
---                   1
---                   0
---          - * x
---              y
---            p
---    5
---    6
---    42
---
--- →
---
--- 42
--- 1
--- Right (Right -12)
---
 
 module Z where
 
